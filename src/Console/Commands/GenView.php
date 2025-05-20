79391af71afd481fffe36aeeef55bdfb566e3e0a
wanganlin <inbox@wanganlin.com>
2025-05-20 13:33:08 +0800

    refactor: 更新管理视图模板

--- src/Console/Commands/GenView.php
+++ src/Console/Commands/GenView.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Juling\DevTools\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Str;
 use Juling\DevTools\Facades\GenerateStub;
+use Juling\DevTools\Support\DevConfig;
 use Juling\DevTools\Support\SchemaTrait;
 use Juling\DevTools\Support\StrHelper;
 
@@ -35,39 +37,41 @@ class GenView extends Command
     {
         $tables = $this->getTables($this->option('prefix'), $this->option('table'));
         foreach ($tables as $table) {
-            $tableName = Str::studly($this->getSingular($table['name']));
-            $groupName = $this->getTableGroupName(Str::snake($tableName));
-            $viewName = Str::substr($tableName, Str::length($groupName));
+            $tableName = $this->getSingular($table['name']);
+
+            $distDir = $this->getTableGroupName($tableName);
+            $subDir = Str::substr($tableName, Str::length($distDir)+1);
+            if (!empty($subDir)) {
+                $distDir = Str::camel($distDir).'/'.Str::camel($subDir);
+            } else {
+                $distDir = Str::camel($distDir);
+            }
 
             $comment = StrHelper::rtrim($table['comment'], '表').'模块';
             $columns = $this->getTableColumns($table['name']);
 
-            $this->tpl($groupName, $viewName, 'Index', $comment, $columns, 'index');
-            $this->tpl($groupName, $viewName, 'Create', $comment, $columns, 'create');
-            $this->tpl($groupName, $viewName, 'Edit', $comment, $columns, 'edit');
+            $this->tpl($distDir, $tableName, 'Index', $comment, $columns, 'index');
+            $this->tpl($distDir, $tableName, 'Upsert', $comment, $columns, 'upsert');
         }
     }
 
-    private function tpl(string $groupName, string $viewName, string $name, string $comment, array $columns, string $view): void
+    private function tpl(string $distDir, string $tableName, string $name, string $comment, array $columns, string $view): void
     {
-        $dist = resource_path('admin/src/views/'.Str::camel($groupName));
-        if (!empty($viewName)) {
-            $dist .= '/'.Str::camel($viewName);
-        }
+        $dist = resource_path('admin/src/views/'.$distDir);
         $this->ensureDirectoryExists($dist);
 
         $viewFile = $dist.'/'.$name.'View.vue';
         if (! file_exists($viewFile) || $this->option('force')) {
-            GenerateStub::from(__DIR__.'/stubs/view/'.$view.'.stub')
-                ->to($dist)
-                ->name($name.'View')
-                ->ext('vue')
-                ->replaces([
-                    'groupName' => $groupName,
-                    'viewName' => $viewName,
-                    'comment' => $comment,
-                ])
-                ->generate();
+            $content = file_get_contents(__DIR__ . '/stubs/view/'.$view.'.stub');
+            $render = Blade::render($content, [
+                'camelName' => Str::camel($tableName), // userAccount
+                'snakeName' => Str::snake($tableName), // user_account
+                'studlyName' => Str::studly($tableName), // UserAccount
+                'primaryKey' => $this->getTablePrimaryKey($tableName),
+                'comment' => $comment,
+                'columns' => $columns,
+            ]);
+            file_put_contents($viewFile, $render);
         }
     }
 }

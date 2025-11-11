460c5e0235758e8c2db426184fe7771e534a930a
wanganlin <inbox@wanganlin.com>
2025-11-11 15:22:15 +0800

    refactor: 更新路由生成器

--- src/Resolvers/ServiceResolver.php
+++ src/Resolvers/ServiceResolver.php
@@ -29,14 +29,14 @@ class ServiceResolver extends Foundation
         $content = Blade::render($tpl, $data, deleteCachedView: true);
         file_put_contents($dist . '/' . $data['className'] . 'BundleService.php', "<?php\n\n" . $content);
 
-        $this->bundleService($devConfig, $data);
+        $this->baseService($devConfig, $data);
     }
 
     private function baseService(DevConfig $devConfig, array $data): void
     {
         $data['groupName'] = $this->getTableGroupName($data['tableName']);
         $data['namespace'] = "App\\Services\\".$data['groupName'];
-        $dist = $devConfig->getDist('app/Services/'.$data['groupName'].'/Services');
+        $dist = $devConfig->getDist('app/Services/'.$data['groupName']);
         $this->ensureDirectoryExists($dist);
 
         if ($devConfig->getMultiModule()) {
